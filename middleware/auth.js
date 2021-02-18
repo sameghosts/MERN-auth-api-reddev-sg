eba7ed897c514b8c17fb48239e10f02778efa3c7
sameghosts <seanfeiner.brown@gmail.com>
2021-02-18 10:09:32 -0800

    auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,6 +4,9 @@ const passport = require('passport');
 const Strategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const db = require('../models');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 // Construct the Strategy (jwt)
 const options = {
@@ -18,6 +21,29 @@ const findUser = (jwt_payload, done) => {
 const strategy = new Strategy(options, findUser);
 
 // "register" the strategy so passport uses it when we call 'passport.authenticate()' in our routes
+passport.use(strategy);
 
 // intialize passport
+passport.initialize();
 
+// write a function that creates a jwt token 
+const createUserToken = (req, user) => {
+  // check the password from the req.body against the user
+  const validPassword = bcrypt.compareSync(req.body.password, user.password);
+  // if we didn't get a user or the password isn't valid, then throw an error
+  if (!user || !validPassword) {
+    const err = new Error('Invalid Credentials 👮')
+    err.statusCode = 422;
+    throw err
+  } else {
+    return jwt.sign( // otherwise create and sign a new token
+      { id: user._id, user: email },
+      process.env.JWT_SECRET,
+      { expiresIn: '2m' } // TODO: Extend for production
+      );
+  }
+}
+
+module.exports = {
+  createUserToken
+}
